a3da42d2604f4ed556d475162e06fb1ce4431ecc
Kartik Shukla <kartikshukla2301@gmail.com>
2026-07-08 23:20:14 +0530

    Time: 400 ms (90.12%), Space: 57.8 MB (23.46%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -1,40 +1,31 @@
-from typing import List
+MOD = 10 ** 9 + 7
+pow10 = [1] * 100001
+for i in range(1, 100001):
+    pow10[i] = pow10[i - 1] * 10 % MOD
+
 
 class Solution:
     def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
-        MOD = 10**9 + 7
         n = len(s)
+        sum = [0] * (n + 1)
+        x = [0] * (n + 1)
+        count = [0] * (n + 1)
+        for i, c in enumerate(s):
+            d = int(c)
+            sum[i + 1] = sum[i] + d
+            x[i + 1] = (x[i] * 10 + d) % MOD if d > 0 else x[i]
+            count[i + 1] = count[i] + (d > 0)
 
-        pref_sum = [0] * (n + 1)
-        pref_cnt = [0] * (n + 1)
-        pref_val = [0] * (n + 1)
-
-        for i in range(n):
-            d = ord(s[i]) - ord('0')
-            pref_sum[i + 1] = pref_sum[i] + d
-
-            if d:
-                pref_cnt[i + 1] = pref_cnt[i] + 1
-                pref_val[i + 1] = (pref_val[i] * 10 + d) % MOD
-            else:
-                pref_cnt[i + 1] = pref_cnt[i]
-                pref_val[i + 1] = pref_val[i]
-
-        pow10 = [1] * (pref_cnt[-1] + 1)
-        for i in range(1, len(pow10)):
-            pow10[i] = (pow10[i - 1] * 10) % MOD
-
-        ans = []
-
-        for l, r in queries:
-            cnt = pref_cnt[r + 1] - pref_cnt[l]
-            sm = pref_sum[r + 1] - pref_sum[l]
-
-            if cnt == 0:
-                ans.append(0)
-                continue
-
-            x = (pref_val[r + 1] - pref_val[l] * pow10[cnt]) % MOD
-            ans.append((x * sm) % MOD)
+        ans = [0] * len(queries)
+        for i, (l, r) in enumerate(queries):
+            length = count[r + 1] - count[l]
+            ans[i] = (x[r + 1] - x[l] * pow10[length]) * (sum[r + 1] - sum[l]) % MOD
 
         return ans
+        # preprocessing:
+        # time: O(maximum possible string length)
+        # space: O(maximum possible string length)
+        # n = the length of the input string
+        # m = the number of queries
+        # time: O(n + m)
+        # space: O(n)
