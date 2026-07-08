aeff173b2dcc7bd62b94800408678438365f95b5
Kartik Shukla <kartikshukla2301@gmail.com>
2026-07-08 23:22:10 +0530

    Time: 5219 ms (7.74%), Space: 29.6 MB (48.52%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
@@ -1,5 +1,4 @@
 from collections import deque
-import heapq
 
 class Solution:
     def maximumSafenessFactor(self, grid):
@@ -10,33 +9,57 @@ class Solution:
 
         for i in range(n):
             for j in range(n):
-                if grid[i][j] == 1:
+                if grid[i][j]:
                     dist[i][j] = 0
                     q.append((i, j))
 
-        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        dirs = (1, 0, -1, 0, 1)
 
         while q:
             x, y = q.popleft()
-            for dx, dy in dirs:
-                nx, ny = x + dx, y + dy
+            d = dist[x][y] + 1
+            for k in range(4):
+                nx = x + dirs[k]
+                ny = y + dirs[k + 1]
                 if 0 <= nx < n and 0 <= ny < n and dist[nx][ny] == -1:
-                    dist[nx][ny] = dist[x][y] + 1
+                    dist[nx][ny] = d
                     q.append((nx, ny))
 
-        pq = [(-dist[0][0], 0, 0)]
-        seen = [[False] * n for _ in range(n)]
-        seen[0][0] = True
+        def check(limit):
+            if dist[0][0] < limit:
+                return False
 
-        while pq:
-            safe, x, y = heapq.heappop(pq)
-            safe = -safe
+            q = deque([(0, 0)])
+            vis = [[False] * n for _ in range(n)]
+            vis[0][0] = True
 
-            if x == n - 1 and y == n - 1:
-                return safe
+            while q:
+                x, y = q.popleft()
+                if x == n - 1 and y == n - 1:
+                    return True
 
-            for dx, dy in dirs:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < n and 0 <= ny < n and not seen[nx][ny]:
-                    seen[nx][ny] = True
-                    heapq.heappush(pq, (-min(safe, dist[nx][ny]), nx, ny))
+                for k in range(4):
+                    nx = x + dirs[k]
+                    ny = y + dirs[k + 1]
+
+                    if (
+                        0 <= nx < n
+                        and 0 <= ny < n
+                        and not vis[nx][ny]
+                        and dist[nx][ny] >= limit
+                    ):
+                        vis[nx][ny] = True
+                        q.append((nx, ny))
+
+            return False
+
+        lo, hi = 0, n * 2
+
+        while lo < hi:
+            mid = (lo + hi + 1) // 2
+            if check(mid):
+                lo = mid
+            else:
+                hi = mid - 1
+
+        return lo
